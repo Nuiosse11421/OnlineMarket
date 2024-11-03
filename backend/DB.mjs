ef101cc222ff42bcd77578c7d56e642a2b3d4159
Nuiosse11421 <notice.gameacc@gmail.com>
2024-11-03 17:17:48 +0700

    update db

--- backend/DB.mjs
+++ backend/DB.mjs
@@ -1,17 +1,15 @@
 import { MongoClient } from "mongodb";
-
+import mongoose from "mongoose";
 const url = "mongodb+srv://noticegameacc:11473@dbmarket.rs9by.mongodb.net/ONLINEMARKET?retryWrites=true&w=majority&appName=DBMarket";
-const client = new MongoClient(url)
-
+mongoose.set('strictQuery',true);
 
 async function connectDB(){
     try{
-        await client.connect();
-        console.log("Connected to mongodb")
+        await mongoose.connect(url);
+        console.log("MongoDB connected");
     }catch(err){
-        console.error("Failed to connect to MongoDB", err.message);
+        console.error("Failed to connect : ", err);
         throw err;
     }
 }
-
 export default connectDB;
